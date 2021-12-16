4f38dfcc61071f534caeb89f5f6b20b02cbbe309
saadsardar <k180228@nu.edu.pk>
2021-12-16 17:30:07 +0500

    merged code

--- lib/UI/Review/reviews.dart
+++ lib/UI/Review/reviews.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:friendlyeats/UI/Review/display_reviews.dart';
-import 'package:friendlyeats/UI/product/widgets/product_loaded/product_grid.dart';
-import 'package:friendlyeats/bloc/Cart/bloc/cart_bloc.dart';
 import 'package:friendlyeats/data_layer/models/reviews.dart';
 
 class ReviewLoaded extends StatelessWidget {
@@ -11,7 +9,8 @@ class ReviewLoaded extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (reviewList.isEmpty) {
-      return const Text('No Reviews!', style: TextStyle(fontSize: 64));
+      return const SliverToBoxAdapter(
+          child: Text('No Reviews!', style: TextStyle(fontSize: 64)));
     }
     return SliverList(
       delegate: SliverChildBuilderDelegate(
